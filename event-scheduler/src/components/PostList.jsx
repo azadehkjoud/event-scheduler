9e9edbb294bb0f9d96a4c0b046da58040a091e4d
Azadeh <keshavarzjoud@gmail.com>
2024-10-30 21:41:27 +0100

    edit design

--- event-scheduler/src/components/PostList.jsx
+++ event-scheduler/src/components/PostList.jsx
@@ -60,14 +60,14 @@ function PostList() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6 text-center">Blog Posts</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
         {posts.map((post) => (
           <div
             key={post.id}
-            className="bg-white rounded-full shadow-md p-6 hover:shadow-lg transition-shadow duration-200 flex flex-col items-center justify-center text-center w-60 h-60 mx-auto"
+            className="bg-pink-300 rounded-full shadow-md p-6 hover:shadow-lg transition-shadow duration-200 flex flex-col items-center justify-center text-center w-60 h-60 mx-auto"
           >
             {/* <h2 className="text-lg font-semibold mb-2 truncate">{post.title}</h2> */}
-            <p className="text-gray-600 truncate">{post.body.slice(0, 15)}...</p>
+            <p className="text-gray-100 truncate text-white">{post.body.slice(0, 15)}...</p>
             <Link
               to={`/posts/${post.id}`}
               className="text-blue-500 hover:underline mt-4"
