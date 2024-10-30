d2f213746e2e5bec36761252c09e0ab14f652d65
Nesrin D. Tanyeri <daghantanyeri@gmail.com>
2024-10-30 13:22:15 +0100

    added routes

--- event-scheduler/src/App.jsx
+++ event-scheduler/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link, Outlet } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import EventDetailsPage from './pages/EventDetailsPage';
 import SignUpPage from './pages/SignUpPage';
