e9069adc1be834c4443213e655394caaeb3b2da3
Nesrin D. Tanyeri <daghantanyeri@gmail.com>
2024-10-31 11:34:55 +0100

    sign in form completed

--- event-scheduler/src/Pages/SignUpPage.jsx
+++ event-scheduler/src/Pages/SignUpPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/SignUpPage.jsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaGoogle, FaFacebook, FaApple } from 'react-icons/fa'; 
