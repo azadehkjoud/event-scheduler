3b114ef6e34a910f61ff5ac423c01ababa2a2992
Nesrin D. Tanyeri <daghantanyeri@gmail.com>
2024-11-01 17:20:27 +0100

    added video to hero

--- event-scheduler/src/Pages/HomePage.jsx
+++ event-scheduler/src/Pages/HomePage.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import PostList from '../components/PostList';
-import dateScrabble from '../assets/img/agenda.jpg';
+import localVideo from '../assets/img/video.mp4'; // Import your local video
 
 function Home() {
   return (
     <div className="homepage">
-      <div
-        className="hero min-h-[60vh] max-w-7xl mx-auto my-10 rounded-lg shadow-lg overflow-hidden" 
-        style={{
-          backgroundImage: `url(${dateScrabble})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-        <div className="hero-overlay bg-opacity-10"></div>
-        <div className="hero-content text-neutral-content text-center p-8">
-          <div className="max-w-md">
-        
-          </div> 
+      <section className="relative h-[60vh] max-w-7xl mx-auto my-10 rounded-lg shadow-lg overflow-hidden">
+        <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+          <video
+            className="min-w-full min-h-full absolute object-cover"
+            src={localVideo} // Use your local video here
+            type="video/mp4"
+            autoPlay
+            muted
+            loop
+          ></video>
         </div>
-      </div>
+        <div className="video-overlay hero-overlay bg-opacity-60"></div>
+        <div className="video-content text-neutral-content text-center p-8 z-10">
+          <div className="max-w-md mx-auto backdrop-blur-md p-8 rounded-lg shadow-lg space-y-4">
+            <h1 className="text-4xl font-light daisy-font-bold text-white">Full Hero Video</h1>
+            <h3 className="text-2xl font-light text-white">With TailwindCSS & DaisyUI</h3>
+            <button className="btn btn-primary btn-wide daisy-btn rounded-full">Learn More</button>
+          </div>
+        </div>
+      </section>
       <PostList /> {/* Renders PostList on the main page */}
     </div>
   );
 }
 
 export default Home;
-
-
