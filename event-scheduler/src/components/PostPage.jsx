1623f26d519fb0a8c6787b9c41a0ad6a77b0f520
Nesrin D. Tanyeri <daghantanyeri@gmail.com>
2024-11-01 15:46:38 +0100

    updated pages

--- event-scheduler/src/components/PostPage.jsx
+++ event-scheduler/src/components/PostPage.jsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-
 function PostPage() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [post, setPost] = useState(null);
-
   useEffect(() => {
     axios
       .get(`http://localhost:3001/api/events/${id}`)
@@ -17,7 +15,6 @@ function PostPage() {
         console.log(error);
       });
   }, [id]);
-
   return (
     <div className="flex justify-center items-center min-h-screen bg-light-pink"> {/* Light pink background */}
       <div className="bg-pink-300 rounded-full p-8 shadow-lg w-1/2 h-1/2 flex flex-col items-center justify-center"> {/* Bigger circle box */}
@@ -26,8 +23,8 @@ function PostPage() {
             <h1 className="text-3xl font-bold mb-4 text-center">{post.title}</h1>
             <p className="text-gray-700 mb-2 text-center">ID: {post.id}</p>
             <p className="text-gray-600 text-center">{post.description}</p>
-            <button 
-              onClick={() => navigate(-1)} 
+            <button
+              onClick={() => navigate(-1)}
               className="mt-4 p-2 bg-blue-500 text-white rounded" // Centered button
             >
               Back
@@ -40,8 +37,9 @@ function PostPage() {
     </div>
   );
 }
-
 export default PostPage;
+
+
 
 
 
