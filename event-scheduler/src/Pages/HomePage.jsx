dc25588b84ca640d5be011aa91eecfc6107278be
Nesrin D. Tanyeri <daghantanyeri@gmail.com>
2024-11-01 17:22:37 +0100

    refactored code

--- event-scheduler/src/Pages/HomePage.jsx
+++ event-scheduler/src/Pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PostList from '../components/PostList';
-import localVideo from '../assets/img/video.mp4'; // Import your local video
+import localVideo from '../assets/img/video.mp4';
 
 function Home() {
   return (
@@ -9,25 +9,18 @@ function Home() {
         <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
           <video
             className="min-w-full min-h-full absolute object-cover"
-            src={localVideo} // Use your local video here
+            src={localVideo}
             type="video/mp4"
             autoPlay
             muted
             loop
           ></video>
         </div>
-        <div className="video-overlay hero-overlay bg-opacity-60"></div>
-        <div className="video-content text-neutral-content text-center p-8 z-10">
-          <div className="max-w-md mx-auto backdrop-blur-md p-8 rounded-lg shadow-lg space-y-4">
-            <h1 className="text-4xl font-light daisy-font-bold text-white">Full Hero Video</h1>
-            <h3 className="text-2xl font-light text-white">With TailwindCSS & DaisyUI</h3>
-            <button className="btn btn-primary btn-wide daisy-btn rounded-full">Learn More</button>
-          </div>
-        </div>
-      </section>
+         </section>
       <PostList /> {/* Renders PostList on the main page */}
     </div>
   );
 }
 
 export default Home;
+
